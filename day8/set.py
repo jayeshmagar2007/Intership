79d49b611ff87de56027f9bec20c1fb0df321859
jayeshmagar2007 <jayeshmagar2007@gmail.com>
2025-06-12 17:02:23 +0530

    set operation

---
+++ day8/set.py
@@ -0,0 +1,9 @@
+A ={1,2,3,4,9,10}
+B ={5,6,7,8,9,10}
+print("Set 1 :",A)
+print("Set 2 :",B)
+#common elements
+print(A.intersection(B))
+
+print("Elements in A but not in B : ",A-B)
+print("Elements in B but not in A : ",B-A)
