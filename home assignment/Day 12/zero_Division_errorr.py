04425891f431eaf7b8cc1446fdb0e304cbd14f07
jayeshmagar2007 <jayeshmagar2007@gmail.com>
2025-06-20 23:04:19 +0530

    Delete home assignment/Day 12 directory

--- home assignment/Day 12/zero_Division_errorr.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-try:
-    c=a/b
-    print(f"Division is: {c}")
-except ZeroDivisionError:
-    print("cannot divide by zero")
